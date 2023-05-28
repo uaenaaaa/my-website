1f8aa4468aa86edeed7b8aa10e808d5d900604e2
froilan <froilanaquino1@gmail.com>
2023-05-28 15:25:46 +0000

    clean up

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,7 +5,15 @@ import ProjectsSection from '../components/Projects/ProjectsSection'
 const HomePage = ({}) => {
   return (
     <>
-      <div className='w-[95%] md:w-[90%] gap-y-10 flex pt-5 flex-col items-center justify-center'>
+      <div className='
+      w-[95%]
+      md:w-[90%]
+      gap-y-10
+      flex
+      pt-5
+      flex-col
+      items-center
+      justify-center'>
         <HeroSection />
         <ProjectsSection />
       </div>
