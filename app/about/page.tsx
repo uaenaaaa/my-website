745c955e0028b0fc3a818f3fc29d452cc047772a
froilan <froilanaquino1@gmail.com>
2023-05-30 08:37:57 +0000

    added icons and 3 columns on mobile (ongoing)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -30,61 +30,61 @@ const About = () => {
           <h1>Hi, I'm Froilan Aquino, currently living in Bagong Silang, Caloocan City, Manila, Philippines! I'm a full-stack developer/engineer, and I develop and leverage JavaScript, Python, and MySQL.</h1>
         </div>
 
-        <div>
+        <div className='w-full'>
           <div>
             <h1>Contact</h1>
             <p className='flex items-center justify-center'>You have any inquiries? Feel free to <Link className='underline font-bold' href='mailto:froilanaquino1@gmail.com' target='_blank'></Link> me</p>
           </div>
           <div>
-            <div>
+            <div className='w-full'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus omnis ratione nulla cum sit assumenda aspernatur autem? Officia a fugit, necessitatibus autem veritatis voluptatibus non ipsum modi officiis quidem cumque minus nam at vitae sequi in distinctio! Laboriosam voluptates obcaecati voluptatibus unde itaque nemo eligendi magnam modi quisquam nostrum. Consectetur quas maxime voluptate asperiores iure, aliquam sint consequuntur saepe repudiandae reiciendis, pariatur consequatur voluptatem cum dolore explicabo? In a saepe quisquam facere eveniet, quae reiciendis quaerat distinctio cupiditate earum praesentium, harum, impedit nesciunt nemo? Dolores facilis qui cupiditate veritatis ea.
             </div>
 
-            <div>
-              <h1>My TechStack</h1>
+            <div className='w-full flex gap-y-3 flex-start flex-col justify-center'>
+              <h1 className='font-medium'>My TechStack</h1>
 
-              <div>
-                <div className='grid place-content-center gap-2'>
+              <div className='grid w-full grid-cols-3'>
+                <div className='flex flex-col justify-center items-center gap-3'>
                   <SiNextdotjs />
-                  <p>NextJS</p>
+                  <p className='text-md'>NextJS</p>
                 </div>
 
-                <div>
+                <div className='flex flex-col justify-center items-center gap-3'>
                   <FaReact />
                   <p>ReactJS</p>
                 </div>
 
-                <div>
+                <div className='flex flex-col justify-center items-center gap-3'>
                   <SiMysql />
                   <p>MySQL</p>
                 </div>
 
-                <div>
+                <div className='flex flex-col justify-center items-center gap-3'>
                   <SiPostgresql />
                   <p>PostgreSQL</p>
                 </div>
 
-                <div>
+                <div className='flex flex-col justify-center items-center gap-3'>
                   <SiFirebase />
                   <p>Firebase</p>
                 </div>
 
-                <div>
+                <div className='flex flex-col justify-center items-center gap-3'>
                   <SiSupabase />
                   <p>Supabase</p>
                 </div>
 
-                <div>
+                <div className='flex flex-col justify-center items-center gap-3'>
                   <AiFillGithub />
                   <p>Github</p>
                 </div>
 
-                <div>
+                <div className='flex flex-col justify-center items-center gap-3'>
                   <SiTailwindcss />
-                  <p>Tailwind CSs</p>
+                  <p>Tailwind CSS</p>
                 </div>
 
-                <div>
+                <div className='flex flex-col justify-center items-center gap-3'>
                   <CgFigma />
                   <p>Figma</p>
                 </div>
