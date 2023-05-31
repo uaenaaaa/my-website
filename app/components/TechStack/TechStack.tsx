9b8d53e077694c789c915c190779c1ed14e6c4aa
froilan <froilanaquino1@gmail.com>
2023-05-31 10:31:36 +0000

    added id's

--- app/components/TechStack/TechStack.tsx
+++ app/components/TechStack/TechStack.tsx
@@ -12,15 +12,52 @@ import {
 
 const TechStack = () => {
   const TechStackName: string[] = ['NextJs', 'ReactJS', 'MySQL', 'Postgresql', 'Firebase', 'Supabase', 'GitHub', 'Tailwind CSS', 'Figma']
-  const TechStackSVG: JSX.Element[] = [<NextJS />, <ReactJS />, <MySQL />, <Postgresql />, <Firebase />, <Supabase />, <Github />, <TailWindCSS />, <Figma />]
+  const TechStackSVG = [
+    {
+      id: 1,
+      component: <NextJS />
+    },
+    {
+      id: 2,
+      component: <ReactJS />
+    },
+    {
+      id: 3,
+      component: <MySQL />
+    },
+    {
+      id: 4,
+      component: <Postgresql />
+    },
+    {
+      id: 5,
+      component: <Firebase />
+    },
+    {
+      id: 6,
+      component: <Supabase />
+    },
+    {
+      id: 7,
+      component: <Github />
+    },
+    {
+      id: 8,
+      component: <TailWindCSS />
+    },
+    {
+      id: 9,
+      component: <Figma />
+    }
+  ]
   return (
     <>
       <div className='w-full flex gap-y-3 flex-start flex-col justify-center'>
         <h1 className='font-semibold'>My TechStack</h1>
         <div className='grid w-full p-4 gap-7 grid-cols-3'>
           {TechStackSVG.map((svg, index) => (
-            <div className='grid justify-center items-center' key={TechStackName[index]}>
-              {svg}
+            <div className='grid justify-center items-center' key={svg.id}>
+              {svg.component}
               <p className='text-xs justify-self-end self-end'>{TechStackName[index]}</p>
             </div>
           ))}
