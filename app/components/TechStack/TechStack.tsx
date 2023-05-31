1434b48fd86de9dbe8f016938b3942085b0cc00a
froilan <froilanaquino1@gmail.com>
2023-05-31 10:08:46 +0000

    fix key error on .map function

--- app/components/TechStack/TechStack.tsx
+++ app/components/TechStack/TechStack.tsx
@@ -19,7 +19,7 @@ const TechStack = () => {
         <h1 className='font-semibold'>My TechStack</h1>
         <div className='grid w-full p-4 gap-7 grid-cols-3'>
           {TechStackSVG.map((svg, index) => (
-            <div className='grid justify-center items-center' key={index}>
+            <div className='grid justify-center items-center' key={TechStackName[index]}>
               {svg}
               <p className='text-xs justify-self-end self-end'>{TechStackName[index]}</p>
             </div>
