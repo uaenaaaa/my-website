1efa2894f95fb1e0e2ade12372d7a62d85810e79
froilan aquino <froilanaquino1@gmail.com>
2023-06-17 19:41:18 +0800

    added contents for the expertise

---
+++ app/components/Ping.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Ping = () => {
+  return (
+    <>
+      <div className='
+      
+      flex
+      justify-center
+      items-center
+      absolute
+      right-[-5px]
+      top-[-5px]'>
+        
+        <div className='
+        w-4
+        h-4'>
+          <div className='
+          h-full
+          w-full
+          rounded-full
+          bg-[#73648A]'>
+            <div className='
+            w-4
+            h-4
+            rounded-full
+            bg-[#73648A]
+            animate-ping'></div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Ping
--- app/expertise/page.tsx
+++ app/expertise/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Ping from '../components/Ping'
 
 export const metadata = {
   title: 'Expertise | Froilan | Software Engineer',
@@ -11,13 +12,38 @@ const Expertise = ({}) => {
       <div className='
       w-[95%]
       md:w-[90%]
+      lg:w-[80%]
+      xl:w-[70%]
       flex
       flex-col
-      gap-y-5
-      justify-center
-      items-center'>
-        <h1>Expertise</h1>
-        <div>
+      gap-10
+      justify-start
+      items-start'>
+        <h1 className='
+        font-bold
+        text-3xl
+        text-left
+        lg:font-extrabold
+        md:text-[50px]
+        lg:text-[70px]
+        xl:text-[90px]'>Expertise</h1>
+        <div className='
+        w-full
+        flex
+        flex-col
+        '>
+          <div className='
+          w-full
+          relative'>
+            <div className='
+            bg-white
+            rounded-lg
+            w-full
+            p-5'>
+              <h1 className='text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi cupiditate obcaecati natus. Eligendi labore est culpa, maiores minus accusantium sunt. Officia laborum ipsa laudantium vel quasi quas deserunt unde laboriosam ad recusandae praesentium nam culpa mollitia molestias iste, quod placeat optio inventore? Id laboriosam reiciendis officiis tempore? Neque, aliquid? Cumque.</h1>
+            </div>
+            <Ping />
+          </div>
         </div>
       </div>
     </>
