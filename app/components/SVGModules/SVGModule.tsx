82b60a6bf60b3cdc658db9e22a630809c4ed5d0e
froilan aquino <froilanaquino1@gmail.com>
2023-06-30 19:28:49 +0800

    remove dark mode change in nextjs icon

--- app/components/SVGModules/SVGModule.tsx
+++ app/components/SVGModules/SVGModule.tsx
@@ -38,7 +38,7 @@ export const NextJS = ({className}: SVGModuleProps) => {
     <svg onClick={() => {window.open('https://nextjs.org/', '_blank')}} role='link' aria-label='Link to NextJS' className={`${className} fill-none`} viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg">
       <title>Logo of Next.js</title>
       <desc>The Next.js logo represents a popular React framework for building modern web applications. It features the word &quot;Next&quot; in a bold black font with a white arrow pointing towards the right. The arrow signifies progress and forward movement, indicating the framework&apos;s focus on enabling efficient and seamless development experiences. Clicking on this logo will redirect you to the official Next.js website, where you can explore its powerful features, documentation, and resources for building fast, scalable, and SEO-friendly web applications.</desc>
-      <path clipRule="evenodd" d="M0 7.5C0 3.35786 3.35786 0 7.5 0C11.6421 0 15 3.35786 15 7.5C15 10.087 13.6902 12.3681 11.6975 13.7163L4.90687 4.20942C4.78053 4.03255 4.5544 3.95756 4.34741 4.02389C4.14042 4.09022 4 4.28268 4 4.50004V12H5V6.06027L10.8299 14.2221C9.82661 14.7201 8.696 15 7.5 15C3.35786 15 0 11.6421 0 7.5ZM10 10V4H11V10H10Z" className='fill-black dark:fill-white' fillRule="evenodd"/>
+      <path clipRule="evenodd" d="M0 7.5C0 3.35786 3.35786 0 7.5 0C11.6421 0 15 3.35786 15 7.5C15 10.087 13.6902 12.3681 11.6975 13.7163L4.90687 4.20942C4.78053 4.03255 4.5544 3.95756 4.34741 4.02389C4.14042 4.09022 4 4.28268 4 4.50004V12H5V6.06027L10.8299 14.2221C9.82661 14.7201 8.696 15 7.5 15C3.35786 15 0 11.6421 0 7.5ZM10 10V4H11V10H10Z" className='fill-black' fillRule="evenodd"/>
     </svg>
   )
 }
