6134142aecb7f5a9f0bd1c6e00c4a41581d6760d
froilan aquino <froilanaquino1@gmail.com>
2023-09-12 20:26:00 +0800

    modularized certifications

---
+++ app/components/Certification/CertificationsListPrevHome.ts
@@ -0,0 +1,26 @@
+export const certLists = [
+  {
+    id: 1,
+    title: 'SQL Intermediate',
+    description: 'SoloLearn',
+    link: 'https://drive.google.com/file/d/1HczfVsOcVlQLDeUOW2MTLXNpkrz8fNrV/view?usp=sharing'
+  },
+  {
+    id: 2,
+    title: 'Introduction To CSS',
+    description: 'TESDA Philippines',
+    link: 'https://drive.google.com/file/d/1EF4D8QAUxrG5Arlft3V9neKnZisYR96n/view?usp=drive_link'
+  },
+  {
+    id: 3,
+    title: 'Introduction To Python',
+    description: 'SoloLearn',
+    link: 'https://drive.google.com/file/d/1Hf7SbkfJspb7FfqO4SE585e2GKlrcWxq/view?usp=drive_link'
+  },
+  {
+    id: 4,
+    title: 'Introduction to SQL',
+    description: 'SoloLearn',
+    link: 'https://drive.google.com/file/d/1HdyfGti1Vb9WQxmqqynDn1UQdLzZ905b/view?usp=sharing'
+  },
+]
