45f150e1ac7a0b75a9f4f5e4f0dbc496a4d6b647
froilan <froilanaquino1@gmail.com>
2023-11-03 15:46:23 +0800

    fix links

--- app/components/Socials/Socials.tsx
+++ app/components/Socials/Socials.tsx
@@ -16,8 +16,8 @@ const Socials = ({}) => {
   return (
     <>
       <div className='flex flex-row md:w-1/4 lg:w-1/4 xl:w-[15%] w-full justify-between items-center'>
-        <Link target='_blank' href='https://github.com/flcmm'><AiFillGithub size={25} /></Link>
-        <Link target='_blank' href='https://instagram.com/frln_cm'><AiFillInstagram size={25} /></Link>
+        <Link target='_blank' href='https://github.com/froilaaaaan1'><AiFillGithub size={25} /></Link>
+        <Link target='_blank' href='https://instagram.com/froilancious'><AiFillInstagram size={25} /></Link>
         <Link target='_blank' href='https://linkedin.com/in/froilan-aquino-8249a9192'><AiFillLinkedin size={25} /></Link>
         <Toggler />
       </div>
